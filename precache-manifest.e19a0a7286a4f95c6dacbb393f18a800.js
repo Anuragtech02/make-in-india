aea6ce36f7583be3e4e4e3e4d4a2e16cbd1de0fa
Anurag Pal <apal895@gmail.com>
2020-06-24 18:24:04 +0530

    Updates

---
+++ precache-manifest.e19a0a7286a4f95c6dacbb393f18a800.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423898cd1646165f9f50932a2855b882",
+    "url": "/make-in-india/index.html"
+  },
+  {
+    "revision": "0f289e6850d00a7119c9",
+    "url": "/make-in-india/static/css/main.5c741cde.chunk.css"
+  },
+  {
+    "revision": "70eb3dd48ffccc928451",
+    "url": "/make-in-india/static/js/2.2cbea55e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/make-in-india/static/js/2.2cbea55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f289e6850d00a7119c9",
+    "url": "/make-in-india/static/js/main.1fb009cb.chunk.js"
+  },
+  {
+    "revision": "e9dbb38c1f7adce9e8e5",
+    "url": "/make-in-india/static/js/runtime-main.d6189ffa.js"
+  },
+  {
+    "revision": "996ff9287f32b4d54781eb5042d29ecf",
+    "url": "/make-in-india/static/media/image1.996ff928.jpg"
+  },
+  {
+    "revision": "5eaff8f622568ac381c1b35c89143030",
+    "url": "/make-in-india/static/media/image2.5eaff8f6.jpg"
+  },
+  {
+    "revision": "7607d049bbcab8eae9a2f60cd5f58343",
+    "url": "/make-in-india/static/media/image3.7607d049.jpg"
+  },
+  {
+    "revision": "f209e21aa0c50bdf7e101175a41da933",
+    "url": "/make-in-india/static/media/image4.f209e21a.jpg"
+  },
+  {
+    "revision": "d0e9ee92a6398e6b31b1515c2ec6ab5e",
+    "url": "/make-in-india/static/media/image5.d0e9ee92.jpg"
+  }
+]);
