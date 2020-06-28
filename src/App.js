be9f85fbac347ea7bae1624e156f6dd9f3954f67
Anuragtech02 <apal895@gmail.com>
2020-06-28 14:49:18 +0530

    Initiated routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from "react";
 import { Page } from "@material-ui/core";
 import { TopBar, Appbar, NavBar, Slider, ProductSlider } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <div className={styles.outerContainer}>
-      <div className={styles.topBar}>
-        <TopBar />
+    <Router>
+      <div className={styles.outerContainer}>
+        <div className={styles.topBar}>
+          <TopBar />
+        </div>
+        <div className={styles.appBarContainer}>
+          <Appbar />
+        </div>
+        <div className={styles.navbar}>
+          <NavBar />
+        </div>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
-      <div className={styles.appBarContainer}>
-        <Appbar />
-      </div>
-      <div className={styles.navbar}>
-        <NavBar />
-      </div>
+    </Router>
+  );
+};
+
+export default App;
+
+const Home = () => {
+  return (
+    <>
       <div className={styles.slider}>
         <Slider />
       </div>
       <div>
         <ProductSlider />
       </div>
-    </div>
+    </>
   );
 };
-
-export default App;
--- src/components/ProductSlider/ProductSlider.js
+++ src/components/ProductSlider/ProductSlider.js
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-import {
-  Card,
-  Typography,
-  IconButton,
-  Grid,
-  Button,
-  colors,
-} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Card, Typography, IconButton, Button, Grid } from "@material-ui/core";
 import styles from "./ProductSlider.module.css";
 import axios from "axios";
 import ImageSlide from "../ImageSlide/ImageSlide";
@@ -18,16 +11,27 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const ProductSlider = () => {
-  const [iconColor, setIconColor] = useState(styles.nothing);
+  const [iconColor, setIconColor] = useState("var(--greyIcon)");
 
   const onClickHeart = () => {
-    setIconColor(styles.iconActive);
+    setIconColor("var(--primaryColor)");
+  };
+
+  useEffect(() => {
+    // let left = document.getElementsByClassName("slick-prev");
+    // let right = document.getElementsByClassName("slick-next");
+    // left.style.color = "red";
+  }, []);
+
+  const arrowColor = () => {
+    return <div style={{ display: "block", color: "black" }} />;
   };
 
   const settings = {
     className: "center",
     infinite: false,
     centerPadding: "60px",
+    accebility: true,
     dots: true,
     lazyload: true,
     slidesToShow: 4,
@@ -41,38 +45,50 @@ const ProductSlider = () => {
 
   return (
     <div className={styles.container}>
-      <Slider {...settings}>
+      <Slider {...settings} className={styles.slider}>
         {products.map((product) => {
           return (
-            <Card key={product.productId} className={styles.card}>
-              <div className={styles.thumbnail}>
-                <img src={product.imageUrls[0]} alt={product.company} />
-              </div>
-              <div className={styles.name}>
-                <Typography
-                  className={styles.category}
-                  varivar--primaryColorant="subtitle2"
-                >
-                  {product.category}
-                </Typography>
-                <Typography className={styles.company} variant="subtitle1">
-                  {product.company}
-                </Typography>
-              </div>
-              <div className={styles.priceLike}>
-                <h4>₹{product.price}</h4>
-                <IconButton onClick={onClickHeart} className={styles.likeIcon}>
-                  <i
-                    style={{ color: "black" }}
-                    className={classNames("fas fa-heart")}
-                  />
-                </IconButton>
-              </div>
-              <div className={styles.buttons}>
-                <Button className={styles.amazonBtn}>Amazon</Button>
-                <Button className={styles.flipkartBtn}>Flipkart</Button>
-              </div>
-            </Card>
+            <>
+              <Grid item xs={12} className={styles.grid}>
+                <Card key={product.productId} className={styles.card}>
+                  <div className={styles.thumbnail}>
+                    <img src={product.imageUrls[0]} alt={product.company} />
+                  </div>
+                  <div className={styles.name}>
+                    <Typography className={styles.category} variant="subtitle2">
+                      {product.category}
+                    </Typography>
+                    <Typography className={styles.company} variant="subtitle1">
+                      {product.company}
+                    </Typography>
+                  </div>
+                  <div className={styles.priceLike}>
+                    <h4>₹{product.price}</h4>
+                    <IconButton
+                      onClick={onClickHeart}
+                      className={styles.likeIcon}
+                    >
+                      <i
+                        ref={(el) => {
+                          if (el) {
+                            el.style.setProperty(
+                              "color",
+                              iconColor,
+                              "important"
+                            );
+                          }
+                        }}
+                        className={classNames("fas fa-heart")}
+                      />
+                    </IconButton>
+                  </div>
+                  <div className={styles.buttons}>
+                    <Button className={styles.amazonBtn}>Amazon</Button>
+                    <Button className={styles.flipkartBtn}>Flipkart</Button>
+                  </div>
+                </Card>
+              </Grid>
+            </>
           );
         })}
       </Slider>
