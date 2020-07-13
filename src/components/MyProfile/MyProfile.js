48467062cdb562b147e5400fb4ebf606903e5140
Anuragtech02 <apal895@gmail.com>
2020-07-13 14:29:21 +0530

    Initial layout complete

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -8,10 +8,12 @@ import {
   Paper,
   Card,
   Tooltip,
+  Grid,
 } from "@material-ui/core";
 import styles from "./myProfile.module.css";
 import firebase from "../Firebase";
 import { AuthContext } from "../Auth";
+import classNames from "classnames";
 
 const MyProfile = ({ history }) => {
   const { userId } = useParams();
@@ -53,15 +55,15 @@ const ProfileComponent = ({ currentUser, history, userId, userDetails }) => {
     }
   }
 
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [mobile, setMobile] = useState(null);
-  const [website, setWebsite] = useState(null);
-  const [industry, setIndustry] = useState(null);
-  const [email2, setEmail2] = useState(null);
-  const [mobile2, setMobile2] = useState(null);
-  const [facebook, setFacebook] = useState(null);
-  const [insta, setInsta] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [website, setWebsite] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [email2, setEmail2] = useState("");
+  const [mobile2, setMobile2] = useState("");
+  const [facebook, setFacebook] = useState("");
+  const [insta, setInsta] = useState("");
 
   return (
     <div className={styles.container}>
@@ -76,21 +78,37 @@ const ProfileComponent = ({ currentUser, history, userId, userDetails }) => {
             {/* <h5>Change Photo</h5> */}
           </div>
           <div className={styles.personalDetails}>
+            <div className={styles.inputContainer}>
+              {/* <h5>Name</h5> */}
+              <TextField
+                className={styles.textField}
+                value={name}
+                label={userDetails.displayName}
+                size="small"
+                variant="outlined"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+
+            <div className={styles.inputContainer}>
+              {/* <h5>Email</h5> */}
+              <TextField
+                className={styles.textField}
+                value={email}
+                label={userDetails.email}
+                size="small"
+                variant="outlined"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
             <TextField
               className={styles.textField}
-              value={name}
-              label={userDetails.displayName}
+              value={mobile}
+              label={userDetails.mobile}
               size="small"
               variant="outlined"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              className={styles.textField}
-              value={email}
-              label={userDetails.email}
-              size="small"
-              variant="outlined"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setMobile(e.target.value)}
             />
           </div>
         </div>
@@ -99,40 +117,78 @@ const ProfileComponent = ({ currentUser, history, userId, userDetails }) => {
         <div className={styles.heading}>
           <h2>My Products</h2>
         </div>
-        <div className={styles.titleBar}>
-          <div className={styles.productVisibility}>
-            <h4>Visibility</h4>
-          </div>
-          <div className={styles.imageHeading}>
+        <Grid container className={styles.titleBar}>
+          <Grid item md={1} className={styles.productVisibility}>
+            <h4>Status</h4>
+          </Grid>
+          <Grid item md={2} className={styles.imageHeading}>
             <h4>Image</h4>
-          </div>
-          <div className={styles.nameHeading}>
+          </Grid>
+          <Grid item md={6} className={styles.nameHeading}>
             <h4>Product Name</h4>
-          </div>
-          <div className={styles.priceHeading}>
+          </Grid>
+          <Grid item md={1} className={styles.priceHeading}>
             <h4>Price (₹)</h4>
-          </div>
-          <div className={styles.editProduct}>
+          </Grid>
+          <Grid item md={2} className={styles.editProduct}>
             <h4>Edit Details</h4>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
         <div className={styles.products}>
-          <Card className={styles.product}>
-            <div className={styles.visibility}>
-              <Tooltip title="Show/Hide Product" placement="top">
-                <IconButton className={styles.eye}>
-                  <i className="fas fa-eye"></i>
-                </IconButton>
-              </Tooltip>
-            </div>
-            <div className={styles.productDetails}></div>
-            <div className={styles.editProduct}>
-              <IconButton></IconButton>
-              <IconButton></IconButton>
-            </div>
-          </Card>
+          {products.map((product) => {
+            return <HorizontalProduct key={product.title} product={product} />;
+          })}
         </div>
       </Paper>
     </div>
+  );
+};
+
+const HorizontalProduct = ({ product }) => {
+  const { imageUrls, title, price, headline } = product;
+
+  return (
+    <Card className={styles.individualProduct}>
+      <Grid container className={styles.product}>
+        <Grid item md={1} className={styles.visibility}>
+          <Tooltip title="Show/Hide Product" placement="top">
+            <IconButton className={styles.eye}>
+              <i className="fas fa-eye"></i>
+            </IconButton>
+          </Tooltip>
+        </Grid>
+        <Grid item md={9}>
+          <Grid container className={styles.productDetails}>
+            <Grid item md={3} className={styles.productImage}>
+              <img src={imageUrls[0]} alt={`product-${title}`} />
+            </Grid>
+            <Grid item md={8} className={styles.productName}>
+              <h5>
+                <b>{title}</b>
+                <br />
+                {headline}
+              </h5>
+            </Grid>
+            <Grid item md={1} className={styles.productPrice}>
+              <h5>{price}</h5>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item md={2} className={styles.editProduct}>
+          <Tooltip title="Edit Product" placement="top">
+            <IconButton className={styles.iconContainer}>
+              <i className="fas fa-pencil-alt"></i>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Delete Product" placement="top">
+            <IconButton
+              className={classNames(styles.iconContainer, styles.deleteProduct)}
+            >
+              <i className="fas fa-times"></i>
+            </IconButton>
+          </Tooltip>
+        </Grid>
+      </Grid>
+    </Card>
   );
 };
