1166494fd7e09647c81d71ca2d4388edfeba00eb
Anuragtech02 <apal895@gmail.com>
2020-07-24 16:00:00 +0530

    Shop grid complete and major pages responsive

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -32,13 +32,41 @@ export const Stores = () => {
       title: "Bata",
       para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     },
+    {
+      image:
+        "https://images.unsplash.com/photo-1551446339-1e5c6f164ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+      alt: "first-shop",
+      title: "Aveeno",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "second-shop",
+      title: "Lyra",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "third-shop",
+      title: "Head & Shoulders",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1566421/pexels-photo-1566421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "third-shop",
+      title: "Bata",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
   ];
 
   return (
     <div className={styles.container}>
-      <Grid container spacing={0}>
+      <Grid container spacing={2}>
         {data.map((shop) => (
-          <Grid item md={3} xs={12} className={styles.shopContainer}>
+          <Grid item md={3} sm={6} xs={12} className={styles.shopContainer}>
             <div className={styles.imageContainer}>
               <img src={shop.image} alt={shop.alt} />
             </div>
