5e1ba3055c7bce3528c3edbd6b4ab1d35f349e12
Anuragtech02 <apal895@gmail.com>
2020-09-01 11:54:28 +0530

    Changed store cards style

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Stores.module.css";
-import { Grid, Card } from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 
 export const Stores = () => {
   const data = [
@@ -25,62 +25,67 @@ export const Stores = () => {
       title: "Head & Shoulders",
       para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     },
-    {
-      image:
-        "https://images.pexels.com/photos/1566421/pexels-photo-1566421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      alt: "third-shop",
-      title: "Bata",
-      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1551446339-1e5c6f164ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
-      alt: "first-shop",
-      title: "Aveeno",
-      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      alt: "second-shop",
-      title: "Lyra",
-      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      alt: "third-shop",
-      title: "Head & Shoulders",
-      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1566421/pexels-photo-1566421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      alt: "third-shop",
-      title: "Bata",
-      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
+    // {
+    //   image:
+    //     "https://images.pexels.com/photos/1566421/pexels-photo-1566421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    //   alt: "third-shop",
+    //   title: "Bata",
+    //   para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    // },
+    // {
+    //   image:
+    //     "https://images.unsplash.com/photo-1551446339-1e5c6f164ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+    //   alt: "first-shop",
+    //   title: "Aveeno",
+    //   para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    // },
+    // {
+    //   image:
+    //     "https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    //   alt: "second-shop",
+    //   title: "Lyra",
+    //   para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    // },
+    // {
+    //   image:
+    //     "https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    //   alt: "third-shop",
+    //   title: "Head & Shoulders",
+    //   para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    // },
+    // {
+    //   image:
+    //     "https://images.pexels.com/photos/1566421/pexels-photo-1566421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    //   alt: "third-shop",
+    //   title: "Bata",
+    //   para: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    // },
   ];
 
   return (
     <div className={styles.container}>
       <Grid container spacing={2}>
         {data.map((shop, index) => (
-          <Grid
-            key={index}
-            item
-            md={3}
-            sm={6}
-            xs={12}
-            className={styles.shopContainer}
-          >
-            <div className={styles.imageContainer}>
+          <Grid key={index} item lg={4} md={4} sm={6} xs={12}>
+            <div
+              style={{ backgroundImage: `url(${shop.image})` }}
+              className={styles.store}
+            >
+              <div className={styles.overlayContainer}>
+                <div className={styles.description}>
+                  <h3>{shop.title}</h3>
+                  <p>{shop.para}</p>
+                </div>
+                <div className={styles.btnContainer}>
+                  <Button variant="contained" className={styles.shopNowBtn}>
+                    Shop Now
+                  </Button>
+                </div>
+              </div>
+            </div>
+            {/* <div className={styles.imageContainer}>
               <img src={shop.image} alt={shop.alt} />
-            </div>
-            <div className={styles.description}>
-              <h3>{shop.title}</h3>
-              <p>{shop.para}</p>
-            </div>
+            </div> */}
           </Grid>
         ))}
       </Grid>
